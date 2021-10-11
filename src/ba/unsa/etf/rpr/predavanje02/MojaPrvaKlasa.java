819a34d52270ea49a385f6e50f663c4e8368d228
mkobasevic1 <mkobasevic1@etf.unsa.ba>
2021-10-11 14:48:05 +0200

    Promjena teksta

--- src/ba/unsa/etf/rpr/predavanje02/MojaPrvaKlasa.java
+++ src/ba/unsa/etf/rpr/predavanje02/MojaPrvaKlasa.java
@@ -4,6 +4,6 @@ public class MojaPrvaKlasa {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Dobro došli na RPR");
+        System.out.println("Promjena neka");
     }
 }
